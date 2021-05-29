b6a84415126d1e62557f1af2d2af25c39a11d464
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 13:19:23 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -8,3 +8,18 @@ const sizeFactor : number = 3.9
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
 const deg : number = Math.PI / 4 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    } 
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
